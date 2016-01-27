a1258bc14e74957547b8804daacdd6735ab832ea
Tomaz Muraus <tomaz@tomaz.me>
2016-01-27 20:39:06 +0100

    Disable debug mode.

--- scripts/run-puppet.sh
+++ scripts/run-puppet.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # Note: We enable DEBUG mode by default so we get more useful info
-PUPPET_DEBUG_ARGS="--debug --verbose"
+#PUPPET_DEBUG_ARGS="--debug --verbose"
+PUPPET_DEBUG_ARGS=""
 
 # Command used to invoke puppet run
 COMMAND="/usr/bin/sudo FACTER_installer_running=true ENV=current_working_directory NOCOLOR=true /usr/bin/puprun ${PUPPET_DEBUG_ARGS}"
